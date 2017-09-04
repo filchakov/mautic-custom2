be71ea90e6b1de84020062cafa4f49e9a76345e9
filchakov <filchakov.denis@gmail.com>
2017-09-04 21:53:15 +0300

    fix link to logo

--- resources/views/scaffold-interface/layouts/app.blade.php
+++ resources/views/scaffold-interface/layouts/app.blade.php
@@ -77,12 +77,12 @@
 					<!-- sidebar menu: : style can be found in sidebar.less -->
 					<ul class="sidebar-menu">
 						<li class="header">MAIN NAVIGATION</li>
-						<li class="active treeview">
+						<li class="treeview">
 							<a href="/email">
 								<i class="fa fa-inbox"></i> <span>Emails</span></i>
 							</a>
 						</li>
-						<li class="active treeview">
+						<li class="treeview">
 							<a href="/project">
 								<i class="fa fa-gear"></i> <span>Projects</span></i>
 							</a>
